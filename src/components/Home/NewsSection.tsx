e942357e32f227ec26e34493302727adcf0f7a9f
qlab-eng <ruiyangqin2020@gmail.com>
2025-09-01 17:22:54 -0400

    Update NewsSection.tsx

--- src/components/Home/NewsSection.tsx
+++ src/components/Home/NewsSection.tsx
@@ -4,7 +4,7 @@ import news from "../../data/news.json";
 const NewsSection: React.FC = () => (
     <section className="mb-10">
         <h3 className="text-xl font-semibold text-stone-500 mb-4 text-left">Recent News</h3>
-        <ul className="max-w-2xl mx-auto space-y-3 text-left text-base max-h-70 overflow-y-auto pr-2">
+        <ul className="max-w-3xl mx-auto space-y-3 text-left text-base max-h-70 overflow-y-auto pr-2">
             {news.map((item, idx) => (
                 <li key={idx} className="bg-stone-50 rounded p-3 shadow-sm text-sm">
                     <span className="font-semibold text-stone-400 mr-2">[{item.date}]</span>
