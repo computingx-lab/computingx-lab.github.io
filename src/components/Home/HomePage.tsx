5aa1a5dc5e7354f3d225cd0655f9504a624d199b
qlab-eng <ruiyangqin2020@gmail.com>
2025-10-24 09:44:51 -0400

    Adjust width classes for HomePage layout

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -9,10 +9,10 @@ import AboutVillanovaSection from "./AboutVillanovaSection";
 const HomePage: React.FC = () => (
     <div>
         <div className="flex flex-col md:flex-row md:space-x-8">
-            <div className="w-full md:w-16/30">
+            <div className="w-full md:w-15/30">
                 <IntroSection />
             </div>
-            <div className="w-full md:w-14/30">
+            <div className="w-full md:w-15/30">
                 <NewsSection />
             </div>
         </div>
